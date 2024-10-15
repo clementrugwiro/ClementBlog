4073a4c5f9764f15a0cd706e102992c247b981fc
clementrugwiro <hunkclement@gmail.com>
2024-10-15 09:15:03 +0200

    added the download CV feature

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import './landing.css'
 import bgImg from '../../assets/backImage.png'
-import {Link} from 'react-scroll'
+
 import hireme from '../../assets/hire.png'
+import cv from "../../assets/CV.pdf"
 
 const  Landing = () => {
   return (
-    <section id='landing'>
-        <div className='IntroContent'>
-            <span className='hello'>Hello </span> 
-            <span className='introText'>I'm <span className='introName'>Clement</span><br/>an IT professional.</span>
-            <p className='introPara'> I'm a skilled IT professional with <br/> experience in multidisciplinary projects</p>
-            <Link> <button className='btn'><img className='hireme' src={hireme} alt=''/>Hire me</button></Link>
-        </div>
-        <img src={bgImg} alt='' className='bg'/>
+    <section id="landing">
+      <div className="IntroContent">
+        <span className="hello">Hello </span>
+        <span className="introText">
+          I'm <span className="introName">Clement</span>
+          <br />
+          an IT professional.
+        </span>
+        <p className="introPara">
+          {" "}
+          I'm a skilled IT professional with <br /> experience in
+          multidisciplinary projects
+        </p>
+          <a href={cv} download="Your_CV.pdf">
+            {" "}
+            <button className="btn">
+              <img className="hireme" src={hireme} alt="" />
+               Get CV
+            </button>
+          </a>
+      </div>
+      <img src={bgImg} alt="" className="bg" />
     </section>
-  )
+  );
 }
 
 export default Landing
